6885530f4c06ba48a44d76016a368bd86f01f744
Pradeep G <itsprdp@gmail.com>
2013-11-12 18:14:26 +0530

    gawk libyaml-dev libsqlite3-dev sqlite3 libgdbm-dev libncurses5-dev libtool pkg-config libffi-dev

---
+++ cookbooks/rvm_req/recipes/default.rb
@@ -0,0 +1,36 @@
+package "gawk" do
+  action :install
+end
+
+package "libyaml-dev" do
+  action :install
+end
+
+package "libsqllite3-dev" do
+  action :install
+end
+
+package "sqllite3" do
+  action :install
+end
+
+package "libgdbm-dev" do
+  action :install
+end
+
+package "libncurses5-dev" do
+  action :install
+end
+
+package "libtool" do
+  action :install
+end
+
+package "pkg-config" do
+  action :install
+end
+
+package "libffi-dev" do
+  action :install
+end
+
